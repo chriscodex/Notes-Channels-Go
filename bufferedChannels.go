2d534531b3e2800774a55cd428fd858b9c17467a
Christian <3003html@gmail.com>
2022-06-25 21:40:14 -0500

    Iteration created for wg

--- bufferedChannels.go
+++ bufferedChannels.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func doSomething(i int) {
-	fmt.Printf("Started %d", i)
+func doSomething(i int, c chan int, wg *sync.WaitGroup) {
+	fmt.Printf("Started %d\n", i)
 	time.Sleep(2 * time.Second)
-	fmt.Printf("Finished %d", i)
-
+	fmt.Printf("Finished %d\n", i)
+	defer wg.Done()
 }
 
 func main() {
-
+	c := make(chan int)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		go doSomething(i, c, &wg)
+		wg.Add(1)
+	}
+	wg.Wait()
 }
