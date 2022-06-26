a1e7e0757fdf48e3ad0e014bdf372a95fe103081
Christian <3003html@gmail.com>
2022-06-26 15:34:50 -0500

    Function main finished

--- workerPools.go
+++ workerPools.go
@@ -3,14 +3,30 @@ package main
 import "fmt"
 
 func main() {
-
+	tasks := []int{10, 18, 22, 13, 24, 25, 16}
+	numberWorkers := 3
+	jobs := make(chan int, len(tasks))
+	results := make(chan int, len(tasks))
+	// Go routines
+	for i := 0; i < numberWorkers; i++ {
+		go worker(i+1, jobs, results)
+	}
+	// Sending numbers
+	for _, value := range tasks {
+		jobs <- value
+	}
+	close(jobs)
+	// Show values
+	for r := 0; r < len(tasks); r++ {
+		<-results
+	}
 }
 
 func worker(id int, jobs <-chan int, results chan<- int) {
 	for job := range jobs {
 		fmt.Printf("Worker id: %d, started double of %d\n", id, job)
 		dou := doubles(job)
-		fmt.Printf("Worker id: %d, job %d and double %d", id, job, dou)
+		fmt.Printf("Worker id: %d, job %d and double %d\n", id, job, dou)
 		results <- dou
 	}
 }
