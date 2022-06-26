2757129003a415882ca32a4faf999e9200b008b0
Christian <3003html@gmail.com>
2022-06-25 21:43:52 -0500

    Channel changed from in to out in doSomething

--- bufferedChannels.go
+++ bufferedChannels.go
@@ -10,16 +10,18 @@ func doSomething(i int, c chan int, wg *sync.WaitGroup) {
 	fmt.Printf("Started %d\n", i)
 	time.Sleep(2 * time.Second)
 	fmt.Printf("Finished %d\n", i)
-	c <- 1
+	<-c
 	defer wg.Done()
 }
 
 func main() {
-	c := make(chan int)
+	c := make(chan int, 2)
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
-		go doSomething(i, c, &wg)
+		c <- 1
 		wg.Add(1)
+		go doSomething(i, c, &wg)
 	}
+	<-c
 	wg.Wait()
 }
