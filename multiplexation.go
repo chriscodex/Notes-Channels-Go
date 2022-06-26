5c93bd2678a125f6587b7e27128b3f663d6f690b
Christian <3003html@gmail.com>
2022-06-26 17:13:22 -0500

    Select created in a for loop

--- multiplexation.go
+++ multiplexation.go
@@ -1,11 +1,26 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go sleeping(2*time.Second, c1, 2)
+	go sleeping(4*time.Second, c2, 4)
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
 }
 
-func sleeping(i time.Duration, c chan int) {
+func sleeping(i time.Duration, c chan int, param int) {
 	time.Sleep(i)
+	c <- param
 }
