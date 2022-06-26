b5cdc5e8db5deab65f0c7699552c3e6265d6157e
Christian <3003html@gmail.com>
2022-06-25 22:04:01 -0500

    Double functions created

--- readAndWriteChannels.go
+++ readAndWriteChannels.go
@@ -4,6 +4,14 @@ func generator(c chan<- int) {
 	for i := 0; i < 10; i++ {
 		c <- i + 1
 	}
+	close(c)
+}
+
+func double(in <-chan int, out chan int) {
+	for value := range in {
+		out <- value
+	}
+	close(out)
 }
 
 func main() {
