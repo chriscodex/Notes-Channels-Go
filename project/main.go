138f14f6fff12048e2e85132379ae1dafc43d67d
Christian <3003html@gmail.com>
2022-06-26 22:57:10 -0500

    Structs job and worker created

--- project/main.go
+++ project/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "time"
+
+type Job struct {
+	Name   string
+	Delay  time.Duration
+	Number int
+}
+
+type Worker struct {
+	Id         int
+	JobQueue   chan Job
+	WorkerPool chan chan Job
+	QuitChan   chan bool
+}
+
 func main() {
 
 }
